0cb42d0f13e208fa934df3aefbcf75117cc573dd
Tommy-Lim <tlim24@gmail.com>
2017-02-16 13:22:47 -0800

    fixed handling single and double quotes

--- process_subtitles.py
+++ process_subtitles.py
@@ -5,8 +5,8 @@ in the file to find out what the problem is.
 
 filename = "pirates_of_silicon_valley.srt"
 """
-# filename = "small_subtitles.srt"
-filename = "pirates_of_silicon_valley.srt"
+filename = "small_subtitles.srt"
+# filename = "pirates_of_silicon_valley.srt"
 result = '['
 my_file = open(filename)
 line = my_file.readline()
@@ -14,9 +14,13 @@ while line is not '':
     duration = my_file.readline().strip()
     result += '{'
     result += f'"duration": "{duration}",'
+
     line1 = my_file.readline().strip()
+    line1 = line1.replace("\"", "\\\"")
     result += f'"line1": "{line1}",'
+
     line2 = my_file.readline().strip()
+    line2 = line2.replace("\"", "\\\"")
     if line2 is not '':
         result += f'"line2": "{line2}"'
         result += '},'
@@ -28,6 +32,7 @@ while line is not '':
         line = my_file.readline()
 result += ']'
 print(result)
+my_file.close()
 
 target = open("subtitles.js", 'w');
 target.write(result);
