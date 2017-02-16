4a98b0f9992ed52d5116204b9e74e8313cffba1c
Tommy-Lim <tlim24@gmail.com>
2017-02-16 13:02:51 -0800

    converts srt file to js subtitle object

--- process_subtitles.py
+++ process_subtitles.py
@@ -5,8 +5,30 @@ in the file to find out what the problem is.
 
 filename = "pirates_of_silicon_valley.srt"
 """
-filename = "small_subtitles.srt"
+# filename = "small_subtitles.srt"
+filename = "pirates_of_silicon_valley.srt"
+result = '['
 my_file = open(filename)
-for line in my_file:
-  line = line.strip()
-  print(line)
+line = my_file.readline()
+while line is not '':
+    duration = my_file.readline().strip()
+    result += '{'
+    result += f'"duration": "{duration}",'
+    line1 = my_file.readline().strip()
+    result += f'"line1": "{line1}",'
+    line2 = my_file.readline().strip()
+    if line2 is not '':
+        result += f'"line2": "{line2}"'
+        result += '},'
+        my_file.readline()
+        line = my_file.readline()
+    else:
+        result += f'"line2": ""'
+        result += ('},')
+        line = my_file.readline()
+result += ']'
+print(result)
+
+target = open("subtitles.js", 'w');
+target.write(result);
+target.close()
