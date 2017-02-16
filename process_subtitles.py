0eec79e6c5c48c2e45cce67e2c39b289f0f1a7f3
Tommy-Lim <tlim24@gmail.com>
2017-02-16 13:34:23 -0800

    fixed single line issue causing error with atom

--- process_subtitles.py
+++ process_subtitles.py
@@ -5,32 +5,33 @@ in the file to find out what the problem is.
 
 filename = "pirates_of_silicon_valley.srt"
 """
-filename = "small_subtitles.srt"
-# filename = "pirates_of_silicon_valley.srt"
-result = '['
+# filename = "small_subtitles.srt"
+filename = "pirates_of_silicon_valley.srt"
+
+result = '[\n'
 my_file = open(filename)
 line = my_file.readline()
 while line is not '':
     duration = my_file.readline().strip()
-    result += '{'
-    result += f'"duration": "{duration}",'
+    result += '{\n'
+    result += f'"duration": "{duration}",\n'
 
     line1 = my_file.readline().strip()
     line1 = line1.replace("\"", "\\\"")
-    result += f'"line1": "{line1}",'
+    result += f'"line1": "{line1}",\n'
 
     line2 = my_file.readline().strip()
     line2 = line2.replace("\"", "\\\"")
     if line2 is not '':
-        result += f'"line2": "{line2}"'
-        result += '},'
+        result += f'"line2": "{line2}"\n'
+        result += '},\n'
         my_file.readline()
         line = my_file.readline()
     else:
-        result += f'"line2": ""'
-        result += ('},')
+        result += f'"line2": ""\n'
+        result += ('},\n')
         line = my_file.readline()
-result += ']'
+result += ']\n'
 print(result)
 my_file.close()
 
